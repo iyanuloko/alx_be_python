57f4583fdcd5759e140bc02d44d7573662c411f6
iyanuloko <iyanuoluwaloko@gmail.com>
2025-07-09 19:06:45 +0100

    Added advanced exercise.

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,7 +1,7 @@
 income = input("Enter your monthly income: ")
 expenses = input("Enter your monthly expenses: ")
-savings = int(income) - int(expenses)
-pro_savings = (savings * 12) + (savings * 12 * 0.05)
-print("Your monthly savings are $" + str(savings))
+monthly_savings = int(income) - int(expenses)
+pro_savings = (monthly_savings * 12) + (monthly_savings * 12 * 0.05)
+print("Your monthly savings are $" + str(monthly_savings))
 print("Projected savings after one year, with interest, is: $" + str(pro_savings))
 
