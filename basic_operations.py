dd2606cb4dbcfddbe20669bd1a6e588d6897ef14
iyanuloko <iyanuoluwaloko@gmail.com>
2025-07-09 18:10:07 +0100

    First two ALX Python exercises

---
+++ basic_operations.py
@@ -0,0 +1,5 @@
+number1 = 10
+number2 = 5
+print("Addition of " + str(number1) + " and " + str(number2) + " is " + str(number1+number2))
+print("Subtraction of " + str(number1) + " and " + str(number2) + " is " + str(number1-number2))
+print("Multiplicaton of " + str(number1) + " and " + str(number2) + " is " + str(number1*number2))
