562101a4e5b93978dc9abeb53f6906048983eb08
iyanuloko <iyanuoluwaloko@gmail.com>
2025-07-17 18:18:33 +0100

    Fifth exercise edit again

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -4,16 +4,16 @@ time_bound = input("Is it time-bound? (yes/no): ")
 match priority:
     case "high":
         if time_bound == "yes":
-            print(task_variable + " is a " + task_priority + " priority task that requires immediate attention today!")
+            print("Reminder: " + task_variable + " is a " + task_priority + " priority task that requires immediate attention today!")
         else:
             print(task_variable + " is a " + task_priority + " priority task that requires attention soon.")
     case "medium":
         if time_bound == "yes":
-            print(task_variable + " is a " + task_priority + " priority task that requires immediate attention!")
+            print("Reminder: " + task_variable + " is a " + task_priority + " priority task that requires immediate attention!")
         else:
             print(task_variable + " is a " + task_priority + " priority task that requires attention soon.")
     case "low":
         if time_bound == "yes":
-            print(task_variable + "is a " + task_priority + " priority task that requires immediate attention later!.")
+            print("Reminder: " + task_variable + "is a " + task_priority + " priority task that requires immediate attention later!.")
         else:
             print(task_variable + " is a " + task_priority + " priority task. Consider completing it when you have free time.")
