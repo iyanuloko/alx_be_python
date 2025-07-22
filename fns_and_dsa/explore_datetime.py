b8f9c78f591a477d2033b349f51ccd887aecc252
iyanuloko <iyanuoluwaloko@gmail.com>
2025-07-22 16:59:08 +0100

    ALX exercises corrections edit

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -3,7 +3,7 @@ from datetime import datetime, timedelta
 
 def display_current_datetime():
     current_date = datetime.now()
-    print(f"Current date and time: {current_date.year}-{current_date.month}-{current_date.day} {current_date.hour}:{current_date.minute}:{current_date.second}")
+    print(f"Current date and time: {current_date.strftime("%Y-%m-%d %H:%M:%S")}")
 
 display_current_datetime()
 number_of_days = int(input("Enter the number of days to add to the current date: "))
@@ -12,7 +12,7 @@ number_of_days = int(input("Enter the number of days to add to the current date:
 def calculate_future_date(number_of_days):
     current_date = datetime.now()
     future_date = current_date + timedelta(days=number_of_days)
-    print(f"Future date: {future_date.year}-{future_date.month}-{future_date.day}")
+    print(f"Future date: {future_date.strftime("%Y-%m-%d")}")
 
 
 print(calculate_future_date(number_of_days))
