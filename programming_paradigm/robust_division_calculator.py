fa21ff235e329ccf0ab79bb30286e5b1eb1d08a1
iyanuloko <iyanuoluwaloko@gmail.com>
2025-08-02 20:36:42 +0100

    Second exercise edit 2

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -5,6 +5,7 @@ def safe_divide(numerator, denominator):
     except ValueError:
         print("Error: Please enter numeric values only.")
     try:
-        return numerator/denominator
+        result = numerator/denominator
+        print(f"The result of the division is {result}")
     except ZeroDivisionError:
         print("Error: Cannot divide by zero.")
