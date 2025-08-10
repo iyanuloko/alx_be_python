93f822de8acc686e21587664d9784e649ffe816e
iyanuloko <iyanuoluwaloko@gmail.com>
2025-08-10 23:49:07 +0100

    Third exercise v1

---
+++ oop/polymorphism_demo.py
@@ -0,0 +1,25 @@
+import math
+
+
+class Shape:
+    def __init__(self):
+        pass
+
+    def area(self):
+        raise NotImplementedError()
+
+class Rectangle(Shape):
+    def __init__(self, length, width):
+        super().__init__()
+        self.length = length
+        self.width = width
+    def area(self):
+        return f"The area of the rectangle is: {self.length * self.width}"
+
+class Circle(Shape):
+    def __init__(self, radius):
+        super().__init__()
+        self.radius = radius
+    def area(self):
+        return f"The radius of the circle is: {math.pi * self.radius * self.radius}"
+
